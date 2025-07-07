dd10c3bb4f11653d99bbbef639edc3c67ed4e8e8
Mahaboobbasha-prince <150207512+Mahaboobbasha-prince@users.noreply.github.com>
2025-07-07 18:20:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Dashboard from "./components/Dashboard";
-// import Profile from "./components/Profile";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Dashboard />} />
-//         <Route path="/profile" element={<Profile />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
